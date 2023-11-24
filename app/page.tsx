8fb73b8cec38e37199a4b69c76489238e647a664
qixiaa <aqixiaa@gmail.com>
2023-11-24 13:37:47 +0800

    hero, guide, camp components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className='font-bold text-teal-500'>
-      Hello World
-    </h1>
-  )
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonPros = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant: string;
 };
 
 const Button = ({ type, title, icon, variant }: ButtonPros) => {
